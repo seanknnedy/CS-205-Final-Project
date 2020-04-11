9b597e4e7de6479daeb2724e81a92ab56da9e773
kefah123 <kefah@uvm.edu>
2020-04-11 15:32:56 -0400

    updated docs

--- PieceTest.java
+++ PieceTest.java
@@ -2,27 +2,32 @@
 public class PieceTest {
     public static boolean testPiece() {
        boolean passed = true;
+       //Testing constructor
        Piece P1 = new Piece("blue",0,0,0);
        if (!P1.getColor().equals("blue") || P1.getX()!=0 || P1.getY()!=0 || P1.getBlot() || P1.getID()!=0) {
            passed = false;
            System.out.println("Failed constructor Test case");
        }
+       //Testing constructor case 2
        Piece P2 = new Piece("blue",4,3,1);
-        if (!P2.getColor().equals("blue") || P2.getX()!=4 || P2.getY()!=3 || P2.getBlot() || P2.getID()!=1) {
-            passed = false;
-            System.out.println("Failed constructor 2 Test case");
+       if (!P2.getColor().equals("blue") || P2.getX()!=4 || P2.getY()!=3 || P2.getBlot() || P2.getID()!=1) {
+           passed = false;
+           System.out.println("Failed constructor Test case 2");
         }
+        //Testing equals
         Piece P3 = new Piece("blue",2,3,1);
        if (!P3.equals(P2)) {
            passed = false;
            System.out.println("Failed equals Test case");
        }
+       //Testing switchBlot
        P2.setBlot(false);
        P2.switchBlot();
        if (!P2.getBlot() || P2.getX()!=0 || P2.getY()!=0) {
            passed = false;
            System.out.println("Failed switchBlot method");
        }
+       //Testing switchBlot case 2
        P2.setBlot(true);
        P2.setX(3);
        P2.setY(5);
@@ -31,10 +36,12 @@ public class PieceTest {
             passed = false;
             System.out.println("Failed switchBlot 2 method");
         }
+       //Testing toString
        if (!P2.toString().equals("blue1")) {
            passed = false;
            System.out.println("Failed toString method");
        }
+       //Testing isSameTeam
        if (!P1.isSameTeam(P3)){
            passed = false;
            System.out.println("Failed isSameTeam method");
--- SpikeTest.java
+++ SpikeTest.java
@@ -1,33 +1,36 @@
 import java.util.*;
+/** This program tests the spike class */
 public class SpikeTest {
     public static boolean testSpike() {
         boolean passed = true;
+        //Testing Constructor
         Spike s1 = new Spike(0, 0);
         if (s1.getX() != 0 || s1.getY() != 0 || !s1.getCurrentTeam().equals("NONE") || s1.getPiecesOnSpike().size() != 0) {
             passed = false;
             System.out.println("Failed constructor test case");
         }
+        //Testing addToSpike
         Piece p1 = new Piece("red", 5, 4, 1);
         s1.addToSpike(p1);
         ArrayList<Piece> testSpike = new ArrayList<Piece>();
         testSpike.add(p1);
-
         if (p1.getX() != 0 || p1.getY() != 0 || !s1.getCurrentTeam().equals(p1.getColor()) ||
                 !s1.getPiecesOnSpike().equals(testSpike)) {
             passed = false;
             System.out.println("Failed addtoSpike test case");
         }
-
+        //Testing addToSpike case 2
         testSpike = new ArrayList<Piece>();
         testSpike.add(p1);
-
         Spike s2 = new Spike(5, 1);
         s2.addToSpike(p1);
+
         if (p1.getX() != 5 || p1.getY() != 1 || !s2.getCurrentTeam().equals(p1.getColor()) ||
                 !s2.getPiecesOnSpike().equals(testSpike)) {
             passed = false;
             System.out.println("Failed addToSpike test case 2");
         }
+        //Testing removeFromSpike
         testSpike = new ArrayList<Piece>();
         s2.removeFromSpike(p1);
         if (p1.getX() != 0 || p1.getY() != 0 || !s2.getCurrentTeam().equals("NONE") ||
@@ -35,6 +38,7 @@ public class SpikeTest {
             passed = false;
             System.out.println("Failed removeFromSpike test case");
         }
+        //Testing addToSpike case 3
         s2 = new Spike(12, 2);
         Piece p2 = new Piece("red", 5, 1, 2);
         testSpike = new ArrayList<Piece>();
@@ -46,27 +50,31 @@ public class SpikeTest {
             passed = false;
             System.out.println("Failed addToSpike test case 3");
         }
+        //Testing removeFromSpike case 2
         testSpike.remove(p2);
         s2.removeFromSpike(p2);
         if (p2.getX() != 0 || p2.getY() != 0 || !s2.getPiecesOnSpike().equals(testSpike)) {
             passed = false;
             System.out.println("Failed removeFromSpike test case 2");
         }
+        //Testing equals
         s2 = new Spike(11, 1);
         Spike s3 = new Spike(11, 1);
         if (!s2.equals(s3)) {
             passed = false;
             System.out.println("Failed equals test case");
         }
+        //Testing toString
         s2.addToSpike(p1);
         if (!(s2.toString().equals(testSpike.toString()))) {
             passed = false;
             System.out.println("Failed toString test case");
-    }
+        }
+        //Testing equals case 2
         s3.addToSpike(p1);
         if (!s2.equals(s3)) {
             passed = false;
-            System.out.println("Failed equals test case");
+            System.out.println("Failed equals test case 2");
         }
 
         return passed;
